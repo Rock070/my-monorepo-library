9eac294dfebbb151635b3b51bda9aba6c5974ec0
Rock070 <a0938130702@gmail.com>
2022-07-16 01:09:15 +0800

    feat: mDropdown

---
+++ package/ui/src/atomic/BasicDropdown.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <select name="fruit" id="">
+      <option value="apple">蘋果</option>
+      <option value="banana">香蕉</option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>

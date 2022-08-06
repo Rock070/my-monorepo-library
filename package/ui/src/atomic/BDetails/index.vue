df7b118279b77fba811ed67364f8a329915ae4f2
Rock070 <a0938130702@gmail.com>
2022-08-06 22:09:58 +0800

    feat: 可以正常使用

---
+++ package/ui/src/atomic/BDetails/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+// import IconArrow from './triangle-arrow.svg'
+import IconBase from '@/components/IconBase.vue'
+</script>
+
+<template>
+  <details ref="detailsEl">
+    <summary
+      class="
+      cursor-pointer
+      flex items-center justify-between
+      font-bold
+    "
+    >
+      <slot
+        name="summary"
+      />
+      <!-- <IconBase
+        width="12"
+        height="10"
+        icon-name="toggle-arrow"
+        icon-title="展開或收合內容"
+        class="icon transition-transform ease-in-out"
+      >
+        <IconArrow />
+      </IconBase> -->
+    </summary>
+    <div class="pt-4">
+      <slot />
+    </div>
+  </details>
+</template>
+
+<style scoped>
+summary {
+  list-style: none;
+}
+
+details[open] {
+  .icon {
+    @apply rotate-180;
+  }
+}
+</style>
---
+++ package/ui/src/atomic/BTree/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    123
+  </div>
+  <!-- <b-tree />
+  123
+  </b-tree> -->
+</template>

1ffb7d70a0d020ddc93564b2297133a06005ed18
Rock070 <a0938130702@gmail.com>
2022-08-09 00:31:50 +0800

    build(windicss): infinity scroll 完成

--- playground/pages/infinity-scroll.vue
+++ playground/pages/infinity-scroll.vue
@@ -2,20 +2,27 @@
 import { ref, reactive, onMounted } from 'vue'
 import { useAsyncData } from '#imports'
 
+interface ImgList {
+  id: string
+  author: string
+  width: number
+  height: number
+  url: string
+  download_url: string
+}
+
 const imgList = reactive([])
 
 const El = ref<null | HTMLDivElement>(null)
 const observerEl = ref<null | HTMLDivElement>(null)
 
 onMounted(() => {
-  const intersectionObserver = new IntersectionObserver(entries => {
-    entries.forEach((item) => {
-      if (item.isIntersecting) {
-        console.debug('進入可視區域')
-        page.value++
-        refresh()
-      }
-    })
+  const intersectionObserver = new IntersectionObserver(([entries]) => {
+    if (entries && entries.isIntersecting) {
+      console.debug('進入可視區域')
+      page.value++
+      refresh()
+    }
   }, {
     threshold: [0],
     root: El.value
@@ -28,13 +35,12 @@ const IMAGE_PER_PAGE = 3
 
 const { refresh, pending } = useAsyncData(() => {
   /* eslint-disable no-undef */
-  return $fetch('https://picsum.photos/v2/list', {
+  return $fetch<ImgList[]>('https://picsum.photos/v2/list', {
     params: {
       page: page.value,
       limit: IMAGE_PER_PAGE
     }
   }).then(res => {
-    console.debug(res)
     imgList.push(...res)
     return res
   })
@@ -44,19 +50,36 @@ const { refresh, pending } = useAsyncData(() => {
 </script>
 
 <template>
-  <div class="w-screen h-screen">
+  <div
+    class="w-screen h-screen flex justify-center py-10"
+  >
     <div
       ref="El"
-      class="overflow-scroll text-centner"
+      class="
+        relative
+        h-80vw w-500px
+        mx-2 overflow-y-auto
+        border-4 border-solid border-fuchsia-200
+      "
     >
       <img
         v-for="item in imgList"
         :key="item.id"
         :src="item.download_url"
       >
-      <div ref="observerEl">
-        bottom {{ pending }}
+      <div
+        ref="observerEl"
+        class="text-center"
+      >
+        碰到底了
+        <!-- TODO: loding 動畫 -->
       </div>
     </div>
   </div>
 </template>
+
+<style>
+html {
+ @apply bg-yellow-200;
+}
+</style>
--- playground/windi.config.ts
+++ playground/windi.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'windicss/helpers'
 
 export default defineConfig({
-  attributify: true,
+  // attributify: true,
   extract: {
     include: ['**/*.{vue,ts}'],
     exclude: ['node_modules', '.git']

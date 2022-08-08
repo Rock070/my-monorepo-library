ba3ba87b4d7e6fb873526123edd1dc9a0e8ed671
Rock070 <a0938130702@gmail.com>
2022-08-09 00:12:50 +0800

    build(windicss): attributify mode on

---
+++ playground/windi.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  attributify: true,
+  extract: {
+    include: ['**/*.{vue,ts}'],
+    exclude: ['node_modules', '.git']
+  }
+})

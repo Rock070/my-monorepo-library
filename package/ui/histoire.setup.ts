7f948e24cbc5612768ea2f007f300266c973edc0
Rock070 <a0938130702@gmail.com>
2022-10-12 02:05:45 +0800

    build(windi): windicss 設定 devtool onDemand

--- package/ui/histoire.setup.ts
+++ package/ui/histoire.setup.ts
@@ -1,2 +1,4 @@
 import './dist/style.css'
 import './histoire.css'
+import 'virtual:windi.css'
+import 'virtual:windi-devtools'
--- package/ui/src/main.ts
+++ package/ui/src/main.ts
@@ -1,5 +1,5 @@
-
 import 'virtual:windi.css'
+import 'virtual:windi-devtools'
 
 import { createApp } from 'vue'
 import App from './App.vue'
